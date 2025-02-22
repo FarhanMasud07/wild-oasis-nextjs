b40f2913347ebbb7ae249ce018aac5eb3924a08e
farhan-masud-1616 <farhan.masud@optimizely.com>
2025-02-23 00:34:18 +0600

    Full wild oasis project initial commit

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useOptimistic } from "react";
+import ReservationCard from "./ReservationCard";
+import { deleteBookingAction } from "@/app/_lib/actions";
+
+function ReservationList({ bookings }) {
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) =>
+      curBookings.filter((booking) => booking.id !== bookingId)
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteBookingAction(bookingId);
+  }
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          onDelete={handleDelete}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tyrfggjtfqaldwemrusm.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+    domains: ["lh3.googleusercontent.com"],
+  },
+  // output: "export", // static site generation (SSG) for deploying app
+};
 
 export default nextConfig;
